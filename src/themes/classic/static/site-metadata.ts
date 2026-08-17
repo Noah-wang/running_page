43b4f6f25080d582ecd962629d72592f6ac7ec16
Noah Wang <893898956@qq.com>
2026-08-16 21:54:14 -0700

    fix: update header links

--- src/themes/classic/static/site-metadata.ts
+++ src/themes/classic/static/site-metadata.ts
@@ -15,22 +15,22 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'Noah Wang Running',
+  siteUrl: `${getBasePath()}/`,
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: 'Noah Wang running page',
   navLinks: [
     {
       name: 'Summary',
       url: `${getBasePath()}/summary`,
     },
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'GitHub',
+      url: 'https://github.com/Noah-wang',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/Noah-wang/running_page',
     },
   ],
 };
